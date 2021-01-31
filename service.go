cf2746a816ada498632cc217483944416ce15bbf
Allison Piovani <apiovani25@gmail.com>
2021-01-31 18:03:11 -0300

    first commit

---
+++ service.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Index)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Ola mundo</h1>")
+}
